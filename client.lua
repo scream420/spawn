6051e5176849edde19410d1a7d190676d90eac47
scream420 <108689193+scream420@users.noreply.github.com>
2022-07-13 02:09:27 +0200

    Create client.lua

---
+++ client.lua
@@ -0,0 +1,10 @@
+local skrrr = 1
+RegisterCommand('spawn', function()
+    if skrrr == 1 then
+        SetEntityCoords(PlayerPedId(), -1037.74, -2738.04, 20.1693, false, false, false, true)
+        skrrr = 2
+    else
+         SetEntityCoords(PlayerPedId(), -1017.74, -2238.04, 25.1693, false, false, false, true)
+		 skrrr = 1
+    end
+end)
